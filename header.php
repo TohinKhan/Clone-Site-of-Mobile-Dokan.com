ed69dfab482ae831803fd71eeab60054ffde267b
Billal Hossan <tohinkhan777@gmail.com>
2022-04-16 02:34:45 +0600

    first commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+   <header style="text-align: center">
+   <img src="picture/logo.png" alt="mobiledokan">
+   </header>
+    
+</body>
+</html>
